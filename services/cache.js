11c83804bbcfb6903879dacd56d30290143872b9
Andrew Patipaksiri <apax714@gmail.com>
2018-04-28 15:16:59 -0700

    implement exec function

--- services/cache.js
+++ services/cache.js
@@ -4,6 +4,9 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
   console.log('im about to run a query');
-
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  });
+  console.log(key);
   return exec.apply(this, arguments);
 };
