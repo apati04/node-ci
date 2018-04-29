d0b53092eb27149abcced6d929fa17de7202ceed
Andrew Patipaksiri <apax714@gmail.com>
2018-04-28 23:28:47 -0700

    testing

--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,43 @@
 const mongoose = require('mongoose');
-
+const redis = require('redis');
+const util = require('util');
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this;
+};
+mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+  // see if we have a value for 'key' in redis
+  const cacheValue = await client.hget(this.hashKey, key);
 
-mongoose.Query.prototype.exec = function() {
-  console.log('im about to run a query');
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
-  console.log(key);
-  return exec.apply(this, arguments);
+  // if true return
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+  // otherwise, issue query and store in redis
+  const result = await exec.apply(this, arguments);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
