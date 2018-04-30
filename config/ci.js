ce8a9a7e2bada70346d17424c2c990ae2f8476b2
Andrew Patipaksiri <apax714@gmail.com>
2018-04-30 10:23:34 -0700

    server configuration for travis ci

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '1067283322634-cbcl1pmqb4c76118ktak0b16v9tdliib.apps.googleusercontent.com',
+  googleClientSecret: 'DXJZEdncGC_YrJhXb2ToFqqu',
+  mongoURI: 'mongodb://127.0.0.1:27017',
+  cookieKey: 'aspduhfailweuf23',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
